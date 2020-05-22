b89ca80e3cd47e7e221b2388f27298997b986f2a
timtyler <tim@tt1.org>
2020-05-22 11:25:53 -0400

    Clean up node list

--- source/com/springie/elements/nodegrid/GridOfBinsForCachedNodes.java
+++ source/com/springie/elements/nodegrid/GridOfBinsForCachedNodes.java
@@ -17,25 +17,6 @@ public final class GridOfBinsForCachedNodes {
 
   NodeList[][][] node_list;
 
-  //static int temp3, temp4, temp5, temp6;
-
-  //private GridOfBinsForCachedNodes() {
-    //...
-  //}
-
-  // l = log2 of the size of each bin in internal pixels...
-//  public GridOfBinsForCachedNodes(int l) {
-//    this.log2binsize = l;
-//
-//    this.nx = ((Coords.x_pixels << Coords.shift) >> this.log2binsize) + 1;
-//    this.ny = ((Coords.y_pixels << Coords.shift) >> this.log2binsize) + 1;
-//    this.ny = ((Coords.z_pixels << Coords.shift) >> this.log2binsize) + 1;
-//
-//    //Log.log("NodeGrid: Resized... new grid - X: " + nx + " Y:" + ny + " Z:" + nz);
-//
-//    set_up_grid();
-//  }
-
   public void reset(int l) {
     this.log2binsize = l;
 
@@ -43,10 +24,10 @@ public final class GridOfBinsForCachedNodes {
     this.ny = ((Coords.y_pixels << Coords.shift) >> this.log2binsize) + 1;
     this.ny = ((Coords.z_pixels << Coords.shift) >> this.log2binsize) + 1;
 
-    set_up_grid();
+    setUp();
   }
 
-  void set_up_grid() {
+  void setUp() {
     this.node_list = new NodeList[this.nx][this.ny][this.nz];
 
     for (int bin_x = 0; bin_x < this.nx; bin_x++) {
@@ -66,8 +47,6 @@ public final class GridOfBinsForCachedNodes {
   }
 
   public void removeFromList(int x, int y, int z, Node agent) {
-    // sort this bug out!
-
     if ((x < this.nx) && (x >= 0)) {
       if ((y < this.ny) && (y >= 0)) {
         if ((z < this.nz) && (z >= 0)) {
@@ -77,81 +56,11 @@ public final class GridOfBinsForCachedNodes {
     }
   }
 
-//  static final void draw() {
-//    for (int bin_z = nz; --bin_z >= 0;) {
-//      for (int bin_y = ny; --bin_y >= 0;) {
-//        for (int bin_x = nx; --bin_x >= 0;) {
-//          node_list[bin_x][bin_y][bin_z].draw();
-//        }
-//      }
-//    }
-//  }
-
-//  static final void processAndRender() {
-//    final int r = rnd.nextInt() & 3;
-//    switch (r) {
-//      case 0 :
-//        processAndRender0();
-//        break;
-//      case 1 :
-//        processAndRender1();
-//        break;
-//      case 2 :
-//        processAndRender2();
-//        break;
-//      case 3 :
-//        processAndRender3();
-//        break;
-//      default:
-//        throw new RuntimeException("");
-//    }
-//  }
-//
-//  static final void processAndRender0() {
-//    for (int bin_z = nz; --bin_z >= 0;) {
-//      for (int bin_y = 0; bin_y > ny; bin_y++) {
-//        for (int bin_x = nx; --bin_x >= 0;) {
-//          node_list[bin_x][bin_y][bin_z].processAndRender();
-//        }
-//      }
-//    }
-//  }
-//
-//  static final void processAndRender1() {
-//    for (int bin_z = nz; --bin_z >= 0;) {
-//      for (int bin_y = 0; bin_y > ny; bin_y++) {
-//        for (int bin_x = 0; bin_x > ny; bin_x++) {
-//          node_list[bin_x][bin_y][bin_z].processAndRender();
-//        }
-//      }
-//    }
-//  }
-//
-//  static final void processAndRender2() {
-//    for (int bin_z = nz; --bin_z >= 0;) {
-//      for (int bin_y = ny; --bin_y >= 0;) {
-//        for (int bin_x = 0; bin_x > ny; bin_x++) {
-//          node_list[bin_x][bin_y][bin_z].processAndRender();
-//        }
-//      }
-//    }
-//  }
-//
-//  static final void processAndRender3() {
-//    for (int bin_z = nz; --bin_z >= 0;) {
-//      for (int bin_y = ny; --bin_y >= 0;) {
-//        for (int bin_x = nx; --bin_x >= 0;) {
-//          node_list[bin_x][bin_y][bin_z].processAndRender();
-//        }
-//      }
-//    }
-//  }
-//
   void dumpBins() {
     for (int bin_x = 0; bin_x < this.nx; bin_x++) {
       for (int bin_y = 0; bin_y < this.ny; bin_y++) {
         for (int bin_z = 0; bin_z < this.nz; bin_z++) {
-          Log.log(" " + this.node_list[bin_x][bin_y][bin_z].current_number);
+          Log.log(" " + this.node_list[bin_x][bin_y][bin_z].size());
         }
 
         Log.log(" ");
